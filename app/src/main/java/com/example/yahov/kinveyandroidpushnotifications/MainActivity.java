f2336fb955ae3159f80bc3bbe38ac49e776088f1
bilger-progress <bilger.yahov@progress.com>
2018-12-23 23:30:20 +0200

    Set-up connection to Kinvey BaaS. Ping.

--- app/src/main/java/com/example/yahov/kinveyandroidpushnotifications/MainActivity.java
+++ app/src/main/java/com/example/yahov/kinveyandroidpushnotifications/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.yahov.kinveyandroidpushnotifications;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.kinvey.android.Client;
+import com.kinvey.android.callback.KinveyPingCallback;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Client kinveyClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set-up Kinvey Backend.
+        kinveyClient = new Client.Builder(this).build();
+        // Ping Kinvey backend to check connection.
+        kinveyClient.ping(new KinveyPingCallback() {
+            public void onFailure(Throwable t) {
+                Toast.makeText(getApplicationContext(), "Exception was thrown. Check logs.", Toast.LENGTH_LONG).show();
+                System.out.println("Exception was thrown: " + t.getMessage());
+            }
+
+            public void onSuccess(Boolean b) {
+                Toast.makeText(getApplicationContext(), "Kinvey Ping Response: " + b.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
