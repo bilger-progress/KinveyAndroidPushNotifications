b67cf3aa938bd246bbc290de54e35e3feabffa57
bilger-progress <bilger.yahov@progress.com>
2019-01-25 11:28:16 +0200

    Fix code to apply changes from the latest test build.

--- app/src/main/java/com/example/yahov/kinveyandroidpushnotifications/GCMReceiver.java
+++ app/src/main/java/com/example/yahov/kinveyandroidpushnotifications/GCMReceiver.java
@@ -1,16 +1,17 @@
 package com.example.yahov.kinveyandroidpushnotifications;
 
-import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
 
+import com.kinvey.android.push.KinveyGCMService;
+
 public class GCMReceiver extends WakefulBroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        ComponentName comp = new ComponentName(context.getPackageName(), com.example.yahov.kinveyandroidpushnotifications.GCMService.class.getName());
-        startWakefulService(context, (intent.setComponent(comp)));
+        ComponentName comp = new ComponentName(context.getPackageName(), GCMService.class.getName());
+        KinveyGCMService.enqueueWork(context, (intent.setComponent(comp)), KinveyGCMService.class);
     }
 }
